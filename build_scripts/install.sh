6ea52eb3e34c5ab719f2261f559f882ce2939f74
Clément Dufour <clmnt.dufour@gmail.com>
2025-05-04 17:49:40 +0200

    chore: do not use the absolute binary path

--- build_scripts/install.sh
+++ build_scripts/install.sh
@@ -3,13 +3,13 @@
 set -oue pipefail
 set -x
 
-/usr/bin/dnf5 remove --assumeyes \
+dnf5 remove --assumeyes \
     firefox \
     firefox-langpacks \
     gnome-software-rpm-ostree \
 
 
-/usr/bin/dnf5 install --assumeyes \
+dnf5 install --assumeyes \
     adw-gtk3-theme \
     ddcutil \
     fzf \
@@ -24,16 +24,16 @@ set -x
     zstd \
 
 
-/usr/bin/dnf5 clean all
+dnf5 clean all
 
-/usr/bin/systemctl preset-all
+systemctl preset-all
 
 # Rebuild initramfs to change plymouth theme
 # https://gitlab.com/fedora/ostree/ci-test/-/tree/main
 # https://github.com/coreos/layering-examples/blob/main/initramfs-module/Containerfile
 # https://github.com/ublue-os/main/blob/main/initramfs.sh
 KERNEL_VERSION=$(rpm -q kernel | cut -c 8-)
-/usr/bin/dracut --kver "${KERNEL_VERSION}" --force --add ostree --nostrip \
-    --verbose --no-hostonly --zstd --reproducible \
+dracut --kver "${KERNEL_VERSION}" --force --add ostree --nostrip --verbose \
+    --no-hostonly --zstd --reproducible \
     "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-/usr/bin/chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
